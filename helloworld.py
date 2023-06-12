bab9cbba4c0c5923852ad111094afd9dc8203225
David Pettifor <dpettifo@nd.edu>
2023-06-12 11:00:15 -0400

    topic 5 completed

--- helloworld.py
+++ helloworld.py
@@ -1,1 +1,20 @@
-print("hello world!")
+class Tester:
+    def __init__(self, name):
+        self.name = name
+    
+    def say_hello(self):
+        print(f'Hello {self.name}!')
+    
+    def __say_goodbye(self):
+        print(f'Goodbye {self.name}!')
+
+    def bye(self):
+        self.__say_goodbye()
+
+import copy
+a = Tester('John')
+b = copy.deepcopy(a)
+
+b.name = 'David'
+
+a.say_hello()
